aefcd993c0b8d4c46d0b29c7b70a6b59451f816b
peterlxb <woairuidaren@gmail.com>
2018-01-15 16:08:40 +0800

    add module exports method

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ console.log("starting the app");
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js')
 
-var user = os.userInfo();
+// var user = os.userInfo();
 
-//console.log(user.username);
+// //console.log(user.username);
 
-fs.appendFile("message.txt",`hello ${user.username}`);
+// fs.appendFile("message.txt",`hello ${user.username}`);
+var res = notes.add(3,4);
+console.log(res);
---
+++ notes.js
@@ -0,0 +1,5 @@
+console.log("starting the notes")
+
+module.exports.add = (a,b) => {
+    return a + b;
+}
