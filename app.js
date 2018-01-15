0190cdda0be7493aeb41925f6b9230e2c7f55ad4
peterlxb <woairuidaren@gmail.com>
2018-01-15 15:33:37 +0800

    use require to append file

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+console.log("starting the app");
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+//console.log(user.username);
+
+fs.appendFile("message.txt",`hello ${user.username}`);
