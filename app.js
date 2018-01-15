484ea3f79d3bf0e466bda99076bb3477fa2cd7a6
peterlxb <woairuidaren@gmail.com>
2018-01-15 17:07:51 +0800

    learn process.argv

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js')
+
+var command = process.argv[2];
+//console.log(process.argv);
+
+// process.argv.forEach((val,index) => {
+//     console.log(`${index}: ${val}`);
+// });
+
+function checkArgvs(command){
+    if(command === 'add') {
+        console.log('Add note');
+    } else if (command === 'list') {
+        console.log("list note");
+    } else if (command === 'read') {
+        console.log("reading notes")
+    } else if(command === 'remove') {
+        console.log("removing notes")
+    } else {
+        console.log("Commands not recognized");
+    }
+}
+
+checkArgvs(command);
