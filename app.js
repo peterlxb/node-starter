9cc6e450d5167e452b9a30e38a438bf3e6a430ce
peterlxb <woairuidaren@gmail.com>
2018-01-15 16:28:43 +0800

    add gitignore file

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ console.log("starting the app");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js')
 
+
+console.log(_.isArray(true));
+console.log(_.isArray([]));
 // var user = os.userInfo();
 
 // //console.log(user.username);
 
 // fs.appendFile("message.txt",`hello ${user.username}`);
-var res = notes.add(3,4);
-console.log(res);
+// var res = notes.add(3,4);
+// console.log(res);
