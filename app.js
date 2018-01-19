154ad11d0749890e52835f6c280984d79c73d0f4
peterlxb <peterlxb@gmail.com>
2018-01-19 19:09:20 +0800

    encodeURIComponent

--- app.js
+++ app.js
@@ -14,18 +14,20 @@ const argv = yargs
     .alias('help','h')
     .argv;
 
-
+const encodedAddress = encodeURIComponent(argv.address);
+console.log(encodedAddress);
 
 var url = "http://restapi.amap.com/v3/geocode/regeo?key=4769b220d8198ce6b50c20195650bcd7&location=116.481488,39.990464&poitype=商务写字楼&radius=1000&extensions=all&batch=false&roadlevel=0"
-var url1 = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+var url1 = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
 var url2 = "http://restapi.amap.com/v3/geocode/geo?key=4769b220d8198ce6b50c20195650bcd7&address=滨盛路联通大厦&city=杭州"
 request({
-    url: url1,
+    url: url,
     json:true
 }, (error, response, body) => {
    // console.log('err: ',error);
    // console.log('statusCode: ', response && response.statusCode);
-    if(error) console.log(error);
-    console.log('body: ',JSON.stringify(response));
+    console.log(`Address: ${body.regeocode.formatted_address} `);
+    console.log(`Latitude: ${body.regeocode.addressComponent.streetNumber.location}`);
+    console.log(`Longitude: ${body.regeocode.addressComponent.streetNumber.location}`);
 });
 
