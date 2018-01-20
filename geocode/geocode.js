cd858b8717d75bbd12e967f0f4628f5e8b3208ff
peterlxb <peterlxb@gmail.com>
2018-01-20 14:33:33 +0800

    add weather and geocode to aixos

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,33 +1,27 @@
 const request = require('request');
 
-var url = "http://restapi.amap.com/v3/geocode/regeo?key=4769b220d8198ce6b50c20195650bcd7&location=116.481488,39.990464&poitype=商务写字楼&radius=1000&extensions=all&batch=false&roadlevel=0"
-//var url1 = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
-var url2 = "http://restapi.amap.com/v3/geocode/geo?key=4769b220d8198ce6b50c20195650bcd7&address=滨盛路联通大厦&city=杭州"
-
-const geocodeAddresss = (address,callback) => {
-
-    const encodedAddress = encodeURIComponent(address);
+var geocodeAddress = (address, callback) => {
+    var encodedAddress = encodeURIComponent(address);
 
     request({
-        url: url,
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
         json: true
     }, (error, response, body) => {
-        console.log(body.status === "1");
         if (error) {
-            callback("Unable to connect to Google Server");
-        } else if (body.status === "ZERO_RESULTS") {
-            callback("Unable to find address");
-        } else if (body.status === "1") {
-            callback(undefined,{
-                Address: body.regeocode.formatted_address,
-                Latitude: body.regeocode.addressComponent.streetNumber.location,
-                Longitude: body.regeocode.addressComponent.streetNumber.location
-           })
-      }
-
+            callback('Unable to connect to Google servers.');
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback('Unable to find that address.');
+        } else if (body.status === 'OK') {
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
+        }
     });
-}
+};
+
 
 module.exports = {
-    geocodeAddresss,
+    geocodeAddress,
 }
--- weather/weather.js
+++ weather/weather.js
@@ -1,38 +1,22 @@
 const request = require('request');
 
-// key = 65f2c2cd8ed00bee82d171b37c9804c0
-const weatherAPI = "https://api.darksky.net/forecast/65f2c2cd8ed00bee82d171b37c9804c0/37.4224764,-122.0842499";
-
-// const getWeather =  () => {
-//     request({
-//         url: weatherAPI,
-//         json: true
-//     }, (error, response, body) => {
-//         if (!error && response.statusCode === 200) {
-//             console.log(body.currently.temperature);
-//         } else {
-//             console.log("Unable to fetch the weather");
-//         }
-
-//     });
-// }
-
-const getWeather = (lat,lng,callback) => {
+var getWeather = (lat, lng, callback) => {
     request({
-        url: `https://api.darksky.net/forecast/65f2c2cd8ed00bee82d171b37c9804c0/${lat},${lng}`,
+        url: `https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/${lat},${lng}`,
         json: true
     }, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            callback(undefined,{
-                temperature:body.currently.temperature,
+        if (error) {
+            callback('Unable to connect to Forecast.io server.');
+        } else if (response.statusCode === 400) {
+            callback('Unable to fetch weather.');
+        } else if (response.statusCode === 200) {
+            callback(undefined, {
+                temperature: body.currently.temperature,
                 apparentTemperature: body.currently.apparentTemperature
             });
-        } else {
-            callback("Unable to fetch the weather");
         }
-
     });
-}
+};
 
 module.exports = {
     getWeather,
