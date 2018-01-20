fd216de88b45b99342aedef3d24729520f1f85ee
peterlxb <peterlxb@gmail.com>
2018-01-20 08:41:11 +0800

    abstracting callbacks

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -13,27 +13,11 @@ const argv = yargs
     .help()
     .alias('help','h')
     .argv;
-
-const encodedAddress = encodeURIComponent(argv.address);
-console.log(encodedAddress);
-
-var url = "http://restapi.amap.com/v3/geocode/regeo?key=4769b220d8198ce6b50c20195650bcd7&location=116.481488,39.990464&poitype=商务写字楼&radius=1000&extensions=all&batch=false&roadlevel=0"
-var url1 = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
-var url2 = "http://restapi.amap.com/v3/geocode/geo?key=4769b220d8198ce6b50c20195650bcd7&address=滨盛路联通大厦&city=杭州"
-request({
-    url: url,
-    json:true
-}, (error, response, body) => {
-    console.log(body.status === "1");
-   if(error) {
-       console.log('Unable to connect to Google Server');
-   } else if(body.status === "ZERO_RESULTS"){
-       console.log("Unable to find address");
-   } else if(body.status === "1"){
-       console.log(`Address: ${body.regeocode.formatted_address} `);
-       console.log(`Latitude: ${body.regeocode.addressComponent.streetNumber.location}`);
-       console.log(`Longitude: ${body.regeocode.addressComponent.streetNumber.location}`);
-   } 
     
+geocode.geocodeAddresss(argv.address,(errorMsg,results) => {
+    if(errorMsg){
+      console.log(errorMsg);
+   } else {
+      console.log(JSON.stringify(results,undefined,2));
+  }
 });
-
