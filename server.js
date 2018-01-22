cad453d0e56c7f99c60854c28246fdbe8decfe89
peterlxb <peterlxb@gmail.com>
2018-01-22 09:59:46 +0800

    Rendering Templates with data

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+var hbs = require('hbs');
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',(req,res) => res.send({
-    name:'peterlxb',
-    like:["biking","Cities"]
-}));
+app.get('/',(req,res) => res.render('home.hbs',
+    {
+    PageTitle:'Home Page',
+    message:'Home page here',
+    currentYear:new Date().getFullYear(),
+   }));
+
+app.get('/about', (req, res) => res.render('about.hbs',
+    {
+        PageTitle: 'About Page',
+        message: 'Some hbs text here',
+        currentYear: new Date().getFullYear(),
+    }));
 
 app.post('/',function(req,res){
     res.send('Got a POST request');
