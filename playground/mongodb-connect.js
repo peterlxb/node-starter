98bcc6318fb8b3b2288f12d6ff982a6924bd94c7
peterlxb <peterlxb@gmail.com>
2018-01-25 21:05:56 +0800

    Fetch data and ues ObjectID

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 const assert = require('assert');
-
 // Connection URL
 const url = 'mongodb://127.0.00.1:27017';
 
@@ -17,28 +16,35 @@ MongoClient.connect(url, function(err, client) {
 
     const db = client.db(dbName);
 
-    // db.collection('Todos').insertOne({
-    //     text:'something to do',
-    //     compoleted:false
-    // }, (err,result) => {
-    //     if(err) {
+    // db.collection('Users').insertOne({
+    //     name: 'liuxiaobo',
+    //     age: 27,
+    //     location:'hangzhou'
+    // }, (err, result) => {
+    //     if (err) {
     //         return console.log('Unable to insert todo', err);
     //     }
 
-    //     console.log(JSON.stringify(result.ops,undefined,2));
-    //})
+    //     //console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
 
-    db.collection('Users').insertOne({
-        name: 'peterliu',
-        age: 26,
-        location:'hangzhou'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert todo', err);
-        }
+    /*
+    @@ get unique id 
+    */
+    //  db.collection('Users').find({ 
+    //     _id: new ObjectID('5a69d08ddf819ae63a56e6c0') 
+    //     }).toArray((err, docs) => {
+    //     if (err) {
+    //         console.log("No docs find in collection Users");
+    //     }
+    //     //console.log(typeof docs);
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // });
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    })
+    //  db.collection('Users').find({location:'wuhan'}).count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    //  });
 
     client.close();
 });
