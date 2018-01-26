55f52501a2768e5efe4c067359ae624a2e38d178
peterlxb <peterlxb@gmail.com>
2018-01-26 11:02:42 +0800

    add test to db

--- server/server.js
+++ server/server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { mongoose } = require('./db/mongoose');
-const {Todo} = require('./modles/todo');
-const {User} = require('./modles/User');
+const { Todo } = require('./modles/todo');
+const { User } = require('./modles/User');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res) => {
+
+    // perform the mongo db
     var todo = new Todo({
         text: req.body.text
     });
@@ -17,10 +19,12 @@ app.post('/todos',(req,res) => {
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
-        res.send(e);
+        res.status(400).send(e);
     });
 });
 
 app.listen(3001,() => {
     console.log('Started on port 3001')
 });
+
+module.exports = {app};
---
+++ server/tests/server-test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+const expect = require('chai').expect;
+
+
+const { app } = require('./../server');
+const { Todo } = require('./../modles/todo');
+
+
+//Mocha's hooks before(), after(), beforeEach(), and afterEach()
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+    it('Should create a new todo', (done) => {
+        var text = "Test doto text";
+
+        request(app)
+            .post('/todos')
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).to.equal(text);
+            })
+            .end((err,res) => {
+                if(err) {
+                   return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).to.equal(1);
+                    expect(todos[0].text).to.equal(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should not create todo with invalid body data',(done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err,res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).to.equal(0);
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+})
