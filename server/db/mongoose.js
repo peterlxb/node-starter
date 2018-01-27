9aa3b5fde034a4e426d5605d4a57720c3a260050
peterlxb <peterlxb@gmail.com>
2018-01-27 21:57:33 +0800

    add mongo lab URI path

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 const url = "mongodb://heroku_6b7nwrxh:ksim8qc2aguc4jvecjp828a0u0@ds123556.mlab.com:23556/heroku_6b7nwrxh";
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.00.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.00.1:27017/TodoApp');
 
 module.exports = {
     mongoose
