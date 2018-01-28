3719749d00fbdb0a6312ff26d76dd8649e35de34
peterlxb <peterlxb@gmail.com>
2018-01-28 11:20:08 +0800

    set test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const url = "mongodb://heroku_6b7nwrxh:ksim8qc2aguc4jvecjp828a0u0@ds123556.mlab.com:23556/heroku_6b7nwrxh";
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
 
 module.exports = {
