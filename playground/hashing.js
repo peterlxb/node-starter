6d1130c06a59a84b4f5e7676664caf736f251fa5
peterlxb <peterlxb@gmail.com>
2018-01-29 22:11:56 +0800

    hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const { SHA256 } =  require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
+var password = "123abc!";
 
-var token = jwt.sign(data,'1231bc');
-console.log(token);
+// bcrypt.genSalt(10,(err,salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token,'1231bc');
-console.log("decoded ",decoded);
+var hashedpassword1 = "$2a$10$m1kEgMbvfDO2wyEtL3xcG.BCTaeAJ2e9jao1qaJFNEvHQPXIolkgu";
+var hashedpassword2 = "$2a$10$5NVP34QwQp7eo/q23etJleGOwnn4XduiqqxDocajHgxXWxY0VH3uG"
+bcrypt.compare(password, hashedpassword2, (err,res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id:10
+// };
+
+// var token = jwt.sign(data,'1231bc');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'1231bc');
+// console.log("decoded ",decoded);
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
